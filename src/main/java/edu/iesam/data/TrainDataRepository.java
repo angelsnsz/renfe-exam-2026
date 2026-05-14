b6ccf4b9574cdb1773dd03a1370e2ecd01a0bf82
SSZA <angel.sanzam@educa.jcyl.es>
2026-05-14 16:40:23 +0200

    He modelado Train

---
+++ src/main/java/edu/iesam/data/TrainDataRepository.java
@@ -0,0 +1,34 @@
+package edu.iesam.data;
+
+import edu.iesam.domain.Train;
+import edu.iesam.domain.TrainRepository;
+
+import java.util.ArrayList;
+
+public class TrainDataRepository implements TrainRepository {
+    TrainMemLocalDataSource trainMemLocalDataSource;
+
+    public TrainDataRepository(TrainMemLocalDataSource trainMemLocalDataSource) {
+        this.trainMemLocalDataSource = trainMemLocalDataSource;
+    }
+
+    @Override
+    public void add(Train train) {
+        trainMemLocalDataSource.add(train);
+    }
+
+    @Override
+    public void delete(String id) {
+        trainMemLocalDataSource.delete(id);
+    }
+
+    @Override
+    public ArrayList<Train> getAll() {
+        return trainMemLocalDataSource.findAll();
+    }
+
+    @Override
+    public Train getid(String id) {
+        return trainMemLocalDataSource.findAll().get(0);
+    }
+}
