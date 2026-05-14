e158f8aeee90f7d528184549925c63384a14aff1
SSZA <angel.sanzam@educa.jcyl.es>
2026-05-14 17:03:59 +0200

    He creado la funcionalidad que permite visualizar los trenes.

--- src/main/java/edu/iesam/presentation/TrainView.java
+++ src/main/java/edu/iesam/presentation/TrainView.java
@@ -1,5 +1,20 @@
 package edu.iesam.presentation;
 
-public class TrainView {
+import edu.iesam.data.TrainDataRepository;
+import edu.iesam.data.TrainMemLocalDataSource;
+import edu.iesam.domain.AddTrainUseCase;
+import edu.iesam.domain.GetTrainUseCase;
+import edu.iesam.domain.Train;
 
+import java.util.ArrayList;
+
+public class TrainView {
+    public static void addTrain() {
+        AddTrainUseCase addTrainUseCase = new AddTrainUseCase(new TrainDataRepository(TrainMemLocalDataSource.newInstance()));
+    }
+    public static void getTrain() {
+        GetTrainUseCase getTrainUseCase = new GetTrainUseCase(new TrainDataRepository(TrainMemLocalDataSource.newInstance()));
+        ArrayList<Train> trains = getTrainUseCase.execute();
+        System.out.println("Listado de trenes: " + trains);
+    }
 }
